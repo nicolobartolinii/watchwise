5ddd10fdfe39fc9a10d03d26bdfcc858cd08409d
nickb <nicolobartolinii@gmail.com>
2023-09-07 19:38:54 +0200

    api key

--- WatchWise/Services/Api/APIConstants.swift
+++ WatchWise/Services/Api/APIConstants.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct APIConstants {
-    static let apiKey = ProcessInfo.processInfo.environment["TMDB_API_KEY"]!
+    static let apiKey = "703bd0b2519d5fd488e1070d1adb9502" // ProcessInfo.processInfo.environment["TMDB_API_KEY"]!
 }
